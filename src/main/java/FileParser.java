24c0b6cd79b5d62cfa0a5427817dd605096369da
OstapFilipenko_mea <ostap.filipenko@mea-it.services>
2020-04-19 14:40:11 +0200

    structured my code a little bit)

--- src/main/java/FileParser.java
+++ src/main/java/FileParser.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FileParser {
@@ -21,50 +20,18 @@ public class FileParser {
     // List of all records
     private List<Record_Model> record_models = new ArrayList<>();
 
+
+    public FileParser() {
+        fillLocationList();
+        fillrecordsList();
+    }
+
     public List<Location_Model> getLoations() {
-        try{
-            Reader reader = Files.newBufferedReader(Paths.get("./src/main/resources/CSV-Files/time_series_covid19_confirmed_global.csv"));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-            .withHeader("Province/State", "Country/Region", "Lat", "Long")
-            .withIgnoreHeaderCase()
-            .withTrim());
-
-            for (CSVRecord csvRecord: csvParser) {
-                String provinceState = csvRecord.get("Province/State");
-                String countryRegion = csvRecord.get("Country/Region");
-                String lat = csvRecord.get("Lat");
-                String longtitude = csvRecord.get("Long");
-
-                loations.add(new Location_Model(countryRegion, provinceState, lat, longtitude));
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
         return this.loations;
     }
 
     public List<Record_Model> getRecord_models() {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get("./src/main/resources/CSV-Files/time_series_covid19_confirmed_global.csv"));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-                    .withFirstRecordAsHeader()
-                    .withIgnoreHeaderCase()
-                    .withTrim());
-
-            for (CSVRecord csvRecord: csvParser) {
-                for (String d: getAllDatesBetween("1/22/2020")) {
-                    String date = d;
-                    String countryRegion = csvRecord.get("Country/Region");
-                    String provinceState = csvRecord.get("Province/State");
-                    int ill = Integer.parseInt(csvRecord.get(d));
-
-                    record_models.add(new Record_Model(date, countryRegion, ill, provinceState));
-                }
-            }
-            }catch (Exception e){
-            e.printStackTrace();
-        }
-        return record_models;
+        return this.record_models;
     }
 
     public static List<String> getAllDatesBetween(String startDate) {
@@ -93,4 +60,49 @@ public class FileParser {
         }
         return allDatesString;
     }
+
+    public void fillrecordsList(){
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("./src/main/resources/CSV-Files/time_series_covid19_confirmed_global.csv"));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader()
+                    .withIgnoreHeaderCase()
+                    .withTrim());
+
+            for (CSVRecord csvRecord: csvParser) {
+                for (String d: getAllDatesBetween("1/22/2020")) {
+                    String date = d;
+                    String countryRegion = csvRecord.get("Country/Region");
+                    String provinceState = csvRecord.get("Province/State");
+                    int ill = Integer.parseInt(csvRecord.get(d));
+
+                    record_models.add(new Record_Model(date, countryRegion, ill, provinceState));
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void fillLocationList(){
+        try{
+            Reader reader = Files.newBufferedReader(Paths.get("./src/main/resources/CSV-Files/time_series_covid19_confirmed_global.csv"));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                    .withHeader("Province/State", "Country/Region", "Lat", "Long")
+                    .withIgnoreHeaderCase()
+                    .withTrim());
+
+            for (CSVRecord csvRecord: csvParser) {
+                String provinceState = csvRecord.get("Province/State");
+                String countryRegion = csvRecord.get("Country/Region");
+                String lat = csvRecord.get("Lat");
+                String longtitude = csvRecord.get("Long");
+
+                loations.add(new Location_Model(countryRegion, provinceState, lat, longtitude));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
